70b401fb3e53a2764e83af9324b6a3bbb9f170c4
Ubuntu <ubuntu@ip-172-31-42-232.ec2.internal>
2020-11-20 18:24:43 +0000

    commit

---
+++ area.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Created by Tong Gong
+# Created time Novembver 2020
+# This is the Hello World program with a nice format.
+
+def main():
+    print("4*3={}".format(4*3))
+
+
+if __name__ == "__main__":
+    main()
